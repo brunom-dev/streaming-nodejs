a45ffac2fed80d02e4dd1b8a37eb11c047888eb5
Bruno Macedo <devbrunomacedo@gmail.com>
2025-03-16 13:20:02 -0300

    fix

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ server.delete('/videos/:id', async (request, reply) => {
 const port = process.env.PORT || 3100
 const start = async () => {
      try {
-         server.listen({port})
+         server.listen(port)
      } catch (error) {
          console.log(error)
      }
